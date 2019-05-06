fa17243835d70c7037f710981b16b9375ed5252f
rderenko <roman.derenko@perfectial.com>
2019-05-06 18:02:55 +0300

    initial commit

---
+++ src/main/java/com/example/tran/TranApplication.java
@@ -0,0 +1,13 @@
+package com.example.tran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranApplication.class, args);
+    }
+
+}
